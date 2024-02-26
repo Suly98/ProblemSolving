f7b1da8ff99ea30a7919933d39d992d612a6c8e1
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-02-26 17:18:54 +0400

    added SizeSet

---
+++ HashSet/SizeSet.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class SizeSet {
+    public static void main(String[] args) {
+        Set<Integer> numberSet = new HashSet<>();
+
+        numberSet.addAll(Arrays.asList(1,2,3,4,5,6));
+
+        System.out.println(numberSet.size());
+    }
+}
